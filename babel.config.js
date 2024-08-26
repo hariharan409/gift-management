8bfd078b3857b33cfb4e47a75afe31cc522348f8
hariharan409 <hharan987@gmail.com>
2024-08-26 18:11:47 +0800

    babel update

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "presets": [
     "module:metro-react-native-babel-preset",
-    '@babel/preset-env',
-    '@babel/preset-react'],
+    "@babel/preset-env",
+    "@babel/preset-react"],
   "plugins": ["@babel/plugin-syntax-flow"],
 };
