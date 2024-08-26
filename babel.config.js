ed54ca3f123e79e25296e4687e6319ff16506500
hariharan409 <hharan987@gmail.com>
2024-08-26 18:56:03 +0800

    update babel config

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
-module.exports = {
-  "presets": [
-    "module:metro-react-native-babel-preset",
-    "@babel/preset-env",
-    "@babel/preset-react"],
-  "plugins": ["@babel/plugin-syntax-flow"],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      'babel-preset-expo',
+      ['module:metro-react-native-babel-preset',{loose: true}],
+      "@babel/preset-react"
+    ]
+  };
 };
